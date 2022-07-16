ab237443742ee5e0dc2d06d4dcc4c06fd5bc14b3
cjanie <janiechun@hotmail.fr>
2022-07-16 20:52:45 +0200

    cleaning

--- src/main/java/com/oc_p8/ecommerce/EcommerceApplication.java
+++ src/main/java/com/oc_p8/ecommerce/EcommerceApplication.java
@@ -1,24 +1,13 @@
 package com.oc_p8.ecommerce;
 
-import java.sql.SQLException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.oc_p8.ecommerce.ordercycle.infrastructure.dao.OrderQueryDAO;
 
 @SpringBootApplication
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
-
-		try {
-			new OrderQueryDAO().findOrderById(1L);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 
 }
